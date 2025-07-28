bbf483247ed7031b5fc468912b0081e3054bac8c
Luka Barth <lukadbarth@hotmail.com>
2025-07-28 19:44:46 -0300

    estudos do dia 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { Cabecalho } from "./componentes/cabecalho/cabecalho";
+import { Cabecalho } from './componentes/cabecalho/cabecalho';
+import { Rodape } from './componentes/rodape/rodape';
 
 @Component({
   selector: 'app-root',
-  imports: [Cabecalho],
+  imports: [Cabecalho, Rodape],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('organo');
